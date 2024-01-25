ec357613ba1bbc6516aa2d010c9ea82e4ca37448
Nikita Smiichyk <nikitasmiichyk@gmail.com>
2024-01-25 12:26:49 +0000

    0.2

--- app/src/androidTest/java/com/example/a30daysapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/a30daysapp/ExampleInstrumentedTest.kt
@@ -1,8 +1,16 @@
 package com.example.a30daysapp
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.compose.rememberNavController
+import com.example.a30daysapp.screens.ShowAfrica
+import com.example.a30daysapp.screens.ShowAmerica
+import com.example.a30daysapp.screens.ShowAsia
+import com.example.a30daysapp.screens.ShowEurope
+import com.example.a30daysapp.screens.ShowMainButtons
+import com.example.a30daysapp.screens.ShowOceania
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -14,11 +22,74 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class MainActivityTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.a30daysapp", appContext.packageName)
+    fun mainButtonsTest() {
+        // Test the ShowMainButtons function
+        composeTestRule.setContent {
+            ShowMainButtons(rememberNavController())
+        }
+        composeTestRule
+            .onNodeWithText("EUROPE", useUnmergedTree = true)
+            .assertExists()
+    }
+
+    @Test
+    fun europeScreenTest() {
+        // Test the ShowEurope function
+        composeTestRule.setContent {
+            ShowEurope(rememberNavController())
+        }
+        composeTestRule
+            .onNodeWithText("This is the Europe screen content.", useUnmergedTree = true)
+            .assertExists()
+    }
+
+    @Test
+    fun asiaScreenTest() {
+        // Test the ShowAsia function
+        composeTestRule.setContent {
+            ShowAsia(rememberNavController())
+        }
+        composeTestRule
+            .onNodeWithText("This is the Asia screen content.", useUnmergedTree = true)
+            .assertExists()
+    }
+
+    @Test
+    fun africaScreenTest() {
+        // Test the ShowAfrica function
+        composeTestRule.setContent {
+            ShowAfrica(rememberNavController())
+        }
+        composeTestRule
+            .onNodeWithText("This is the Africa screen content.", useUnmergedTree = true)
+            .assertExists()
+    }
+
+    @Test
+    fun americaScreenTest() {
+        // Test the ShowAmerica function
+        composeTestRule.setContent {
+            ShowAmerica(rememberNavController())
+        }
+        composeTestRule
+            .onNodeWithText("This is the America screen content.", useUnmergedTree = true)
+            .assertExists()
+    }
+
+    @Test
+    fun oceaniaScreenTest() {
+        // Test the ShowOceania function
+        composeTestRule.setContent {
+            ShowOceania(rememberNavController())
+        }
+        composeTestRule
+            .onNodeWithText("This is the Oceania screen content.", useUnmergedTree = true)
+            .assertExists()
     }
 }
